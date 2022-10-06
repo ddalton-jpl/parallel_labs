a57078059c2c1238f1f37febb1e5ea630c1fe9cf
Dillon Dalton <ddalton@Dillons-MacBook-Pro.local>
2022-10-06 09:28:44 -0700

    hwk2 initalial setup

---
+++ ThreadedPrimeHw2Student/src/edu/ewu/ytian/prime/Counter.java
@@ -0,0 +1,14 @@
+package edu.ewu.ytian.prime;
+
+public class Counter {
+	private int c = 0;
+
+    public synchronized void increment( int n) {
+        // write me here
+    }
+
+    public synchronized int total() {
+        // write me here
+        return 0;
+    }
+}
